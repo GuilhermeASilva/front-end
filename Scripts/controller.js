8bfee9530b4316887bcd2a5c07ff628d4718fdf0
Hadnan Basilio <hadnan.basilio@aluno.faculdadeimpacta.com.br>
2020-10-23 17:16:14 -0300

    Implementando e melhorando tela de login

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -1,8 +1,32 @@
 var app = angular.module('mainApp', ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider) {
     $routeProvider
     .when('/', {
-        templateUrl: 'page.html'
-    })
-})
+        templateUrl: '/pages/login.html'
+    })    
+    .when('/dashboard', {
+        resolve: {
+            "check": function($location, $rootScope) {
+                if(!$rootScope.loggedIn) {
+                    $location.path('/');
+                }
+            }
+        },
+        templateUrl: '/Pages/dashboard.html'
+    })    
+    .otherwise( {
+        redirectTo: '/'
+    });
+});
+
+app.controller('loginCtrl', function($scope, $location, $rootScope) {
+    $scope.submit = function() {
+        if($scope.username == 'admin@123.com' && $scope.password == 'admin') {
+            $rootScope.loggedIn = true;
+            $location.path('/dashboard');
+        } else {
+            alert('Credenciais incorretas.');
+        }
+    };
+});
