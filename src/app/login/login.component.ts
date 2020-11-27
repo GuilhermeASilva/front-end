02ec96a5e9fdd05d633c8612ab83ea313f20f121
Hadnan Basilio <hadnan.basilio@aluno.faculdadeimpacta.com.br>
2020-11-27 11:54:07 -0300

    Melhorar a tela de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -18,7 +18,7 @@ export class LoginComponent implements OnInit {
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private notificationToastService : NotificationToastService
+    private notificationToastService: NotificationToastService
   ) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
@@ -31,22 +31,31 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(e?) {
-    if((e && e.key == 'Enter') || !e){
-    let form = this.loginForm.controls;
-    let login = {
-      email: form.email.value,
-      senha: form.senha.value,
-    };
-    this.authService.validaLogin(login).subscribe((res: any) => {
-      if (res && res.token != undefined) {
-        this.authService.login(res.user.id, res.user.email, res.user.tipoUsuario, res.token );
-        this.router.navigate(['/']);
-        // console.log('Resultado do login: ', res);
-        this.notificationToastService.sucesso(new NotificationToastModel('Bem vinde ao sistema!'));
-      } else {
-        this.notificationToastService.erro(new NotificationToastModel('Verifique as credenciais!'));
-      }
-    });
-  }
+    if ((e && e.key == 'Enter') || !e) {
+      let form = this.loginForm.controls;
+      let login = {
+        email: form.email.value,
+        senha: form.senha.value,
+      };
+      this.authService.validaLogin(login).subscribe((res: any) => {
+        if (res && res.token != undefined) {
+          this.authService.login(
+            res.user.id,
+            res.user.email,
+            res.user.tipoUsuario,
+            res.token
+          );
+          this.router.navigate(['/']);
+          // console.log('Resultado do login: ', res);
+          this.notificationToastService.sucesso(
+            new NotificationToastModel('Bem vinde ao sistema!')
+          );
+        } else {
+          this.notificationToastService.erro(
+            new NotificationToastModel('Verifique as credenciais!')
+          );
+        }
+      });
+    }
   }
 }
